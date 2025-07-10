c91d1293d975166f3c669c264a7c963e10fb08dd
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-07-09 22:08:35 -0300

    funções PHP

---
+++ exemplo3numerico.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $num = 537; // Número Inteiro
+        echo $num, " = Número Inteiro<br/><br/>";
+        $num = -13; // Número Negativo
+        echo $num, " = Número Negativo<br/><br/>";
+        $num = 0123; // Número Octal (Equivalente a 83 em decimal)
+        echo $num, " = Número Octal<br/><br/>";
+        $num = 0x1A; // Número Hexadecimal (Equivalente a 26 em decimal)
+        echo $num, " = Número Hexadecimal<br/><br/>";
+        $num = 1.234; //Ponto Flutuante
+        echo $num, " = Ponto Flutuante <br/><br/>";
+        $num = 4e23 ; // Notação Científica 
+        echo $num, " = Notação Científica <br/><br/>";
+    ?>
+    
+</body>
+</html>
---
+++ funcoes_PHP.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        # index 0123456789012345
+        $name = "Stefanie Hatcher";
+        echo $name . "<br>";
+        $length = strlen($name);
+        echo $length . "<br>";
+        $cmp = strcmp($name, "Stefanie Hatcher");
+        echo $cmp . "<br>";
+        $index = strpos($name, "e");
+        echo $index . "<br>";
+        $first = substr($name, 9, 5);
+        echo $first . "<br>";
+        $name = strtoupper($name);
+        echo $name . "<br>";
+    ?>
+
+</body>
+</html>
