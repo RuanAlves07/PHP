cf1633b96a940ea7f6eade4af9172cffd436abab
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-07-15 22:07:21 -0300

    save

---
+++ tabuada.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $i = 1;
+    $t = 1;
+
+    while ($t <= 10) {
+        $resultado = $i * $t;
+        echo " $i X $t = $resultado<br>";
+        $t++;
+        while ($i <= 10){
+
+        
+        }
+    } 
+    ?>
+</body>
+</html>
