d80a8300436e81e87c62d5d3ebfa4db3fc3e5806
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-07-25 18:57:53 -0300

    at

--- Templates/banco.php
+++ Templates/banco.php
@@ -6,35 +6,43 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
+    <?php
         $bdServidor = '127.0.0.1';
         $bdUsuario = 'root';
         $bdSenha = '';
         $bdBanco = 'ruanalves';
         $conexao = mysqli_connect($bdServidor, $bdUsuario, $bdSenha, $bdBanco);
             if (mysqli_connect_errno()) {
-                echo "Problemas para conectar no banco. Verifique os dados!";
+                echo "Problemas para conectar no banco, Verifique os dados!";
                 die();
             }
-            function buscar_tarefas($conexao) {
-                    $sqlBusca = 'SELECT * FROM tarefas';
-                    $resultado = mysqli_query($conexao, $sqlBusca);
-                    $tarefas = array();
-                    while ($tarefa = mysqli_fetch_assoc($resultado)) {
-                        $tarefas[] = $tarefa;
-                    }
-                    return $tarefas;
-                }
-            
-            function gravar_tarefa($conexao, $tarefa){
-                $sqlGravar = "Insert INTO tarefas (nome, descricao, prioridade, prazo, concluida) VALUES ( 
-                '{$tarefas['nome']}',
-                '{$tarefas['descricao']}', 
-                '{$tarefas['prioridade']}',
-                '{$tarefas['prazo']}',
-                '{$tarefas['concluida']}',"
+
+
+        function buscar_tarefas($conexao)
+        {
+            $sqlBusca = 'SELECT * FROM tarefas';
+            $resultado = mysqli_query($conexao, $sqlBusca);
+            $tarefas = array();
+            while ($tarefa = mysqli_fetch_assoc($resultado)) {
+                $tarefas[] = $tarefa;
             }
-            mysqli_query($conexao, $sqlGravar)
+        return $tarefas;
+        }
+
+        function gravar_tarefa($conexao, $tarefa) {
+            $sqlGravar = "
+                INSERT INTO tarefas
+                (nome, descrição, prioridade, prazo, concluido) 
+                VALUES
+                    {
+                        '{$tarefa['nome']}',
+                        '{$tarefa['descrição']}',
+                        '{$tarefa['prazo']}',
+                        '{$tarefa['concluida']}',
+                    }
+            ";
+            mysqli_query($conexao,$sqlGravar);
+        }
     ?>
 
 
