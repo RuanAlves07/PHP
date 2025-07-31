3deebc2e9f6f5a3fd08eee3615673fbd0c0c4e06
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-07-30 21:42:08 -0300

    asd

---
+++ exemploSQLInjection/area_restrita.php
@@ -0,0 +1,7 @@
+<?php
+    echo "<h1> &#128680🚨 Área Restrita 🚨 $#128680</h1>";
+
+    echo "<p>Você não deveria estar aqui, mas consegui acessar devido a uma falha de segurança!</p>";
+
+    echo "<p> Isso mostra como ataques SQL Injection podem comprometer sistemas sem proteção adequada.</p>";
+?>
