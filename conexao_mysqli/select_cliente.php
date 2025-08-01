d1409934c39c2b8c2600b5ae3dfb4c7a67269773
Ruan Augusto Alves <ruanaugust.alves@gmail.com>
2025-08-01 20:50:57 -0300

    asd

--- conexao_mysqli/select_cliente.php
+++ conexao_mysqli/select_cliente.php
@@ -18,10 +18,29 @@ $conexao->close();
 <body>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container">
+    <div class="container"> 
         <a class="navbar-brand" href="index.php">HOME</a>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown">
+                        Navegar
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="insert_cliente.php">Inserir Cliente</a></li>
+                        <li><a class="dropdown-item" href="select_cliente.php">Listar Cliente</a></li>
+                        <li><a class="dropdown-item" href="update_cliente.php">Atualizar Cliente</a></li>
+                        <li><a class="dropdown-item" href="delete_cliente.php">Deletar Cliente</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="back/cadastro.php">Cadastro (Back)</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
     </div>
 </nav>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <div class="container mt-5">
     <h2 class="text-center">Clientes Cadastrados</h2>
